97e979a10a28ac87a9bc40911d6813d00ed66998
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-09-04 14:58:51 +0530

    Create help.py

---
+++ user/help.py
@@ -0,0 +1,13 @@
+
+
+
+
+async def help_handle(client, event):
+    button = "<a href='https://t.me/MiKuXuser_bot/3'>➠ ᴄᴏᴍᴍᴀɴᴅꜱ ꜰᴏʀ ᴜꜱᴇʀʙᴏᴛ</a>"
+    await event.edit(f"Tap the Button below to explore all available commands 👇🏻\n<blockquote>{button}</blockquote>", parse_mode="html")
+  
+  
+  
+
+
+
